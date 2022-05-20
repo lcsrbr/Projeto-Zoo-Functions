8149a3c540e29ec06be1e6f42625a547369f39b5
lcsrbr <93lucasribeiro@gmail.com>
2022-05-20 18:14:48 -0300

    lint ex1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-return ids.length !== 0 ? ids.map((id) => data.species.find((animals) => animals.id === id)) : [];
-
+  return ids.length !== 0 ? ids.map((id) => data.species.find((animals) => animals.id === id)) : [];
 }
 
 module.exports = getSpeciesByIds;
