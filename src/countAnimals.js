b7a6af50c9c5e89cbb8cced8f40e220cf11e7f8a
lcsrbr <93lucasribeiro@gmail.com>
2022-05-23 12:31:48 -0300

    ex5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const specieCount = {};
+    data.species.forEach((specie) => {
+      specieCount[specie.name] = specie.residents.length;
+    });
+    return specieCount;
+  }
+  const specieFind = data.species.find((animals) => animals.name === animal.specie).residents;
+  if (!animal.sex) {
+    return specieFind.length;
+  }
+  return specieFind.filter((specie) => specie.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
