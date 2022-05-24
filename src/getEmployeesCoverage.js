73758765123a30c0a4297537e07101b2af0e4da7
lcsrbr <93lucasribeiro@gmail.com>
2022-05-24 12:28:33 -0300

    ex11 (PELO AMOR DE DEUSSSS)

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,54 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const idsEmpl = data.employees.map((person) => person.id);
+const nameEmpl = data.employees.map((person) => person.firstName);
+const lastNameEmpl = data.employees.map((person) => person.lastName);
+const searchType = (param) => {
+  if (idsEmpl.includes(param.id)) {
+    return data.employees.find((person) => person.id === param.id);
+  }
+  if (nameEmpl.includes(param.name)) {
+    return data.employees.find((person) => person.firstName === param.name);
+  }
+  if (lastNameEmpl.includes(param.name)) {
+    return data.employees.find((person) => person.lastName === param.name);
+  }
+  throw new Error('Informações inválidas');
+};
+
+function getEmployObject(param) {
+  const objEmpl = searchType(param);
+  const speciesId = objEmpl.responsibleFor.map((animal) => animal);
+  const objRetr = {
+    id: objEmpl.id,
+    fullName: `${objEmpl.firstName} ${objEmpl.lastName}`,
+    species: [],
+    locations: [],
+  };
+  for (let i = 0; i < speciesId.length; i += 1) {
+    objRetr.species.push(data.species.find((specie) => specie.id === speciesId[i]).name);
+  } // kkkk depois eu refatoro
+  for (let i = 0; i < speciesId.length; i += 1) {
+    objRetr.locations.push(data.species.find((specie) => specie.id === speciesId[i]).location);
+  } // aiai...
+  return objRetr;
 }
+
+function getAllEmployObject() {
+  const arr = [];
+  data.employees.forEach((employee) => arr.push(getEmployObject({
+    id: employee.id,
+  })));
+  return arr;
+}
+
+function getEmployeesCoverage(param) {
+  if (param === undefined) {
+    return getAllEmployObject();
+  }
+  return getEmployObject(param);
+}
+
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
