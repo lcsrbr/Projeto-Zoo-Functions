4d9822ce8d79e0d873cfa5283a4b7a6d1e369405
lcsrbr <93lucasribeiro@gmail.com>
2022-05-23 21:38:17 -0300

    ex10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,6 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
+  const [firstAnimal] = data.employees.find((person) => person.id === id).responsibleFor; // id do animal
+  const [residents] = data.species.find((animal) => animal.id === firstAnimal).residents
+    .sort((a, b) => b.age - a.age);
+  return Object.values(residents);
   // seu código aqui
 }
 
