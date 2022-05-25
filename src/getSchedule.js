40c70785ee54113bbf016dd8543491e6bda7e436
lcsrbr <93lucasribeiro@gmail.com>
2022-05-25 12:56:49 -0300

    refatorando getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,53 +1,41 @@
 const data = require('../data/zoo_data');
 
-const infos = {
-  Tuesday: {
-    officeHour: 'Open from 8am until 6pm',
-    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
-  },
-  Wednesday: {
-    officeHour: 'Open from 8am until 6pm',
-    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
-  },
-  Thursday: {
-    officeHour: 'Open from 10am until 8pm',
-    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
-  },
-  Friday: {
-    officeHour: 'Open from 10am until 8pm',
-    exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
-  },
-  Saturday: {
-    officeHour: 'Open from 8am until 10pm',
-    exhibition: [
-      'lions', 'tigers',
-      'bears', 'penguins',
-      'otters', 'frogs',
-      'snakes', 'elephants',
-    ],
-  },
-  Sunday: {
-    officeHour: 'Open from 8am until 8pm',
-    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
-  },
-  Monday: {
-    officeHour: 'CLOSED',
-    exhibition: 'The zoo will be closed!',
-  },
-};
+function getWeekSchedule(day) {
+  const animalsArray = data.species.filter((specie) => (specie.availability.includes(day)));
+  if (day === 'Monday') {
+    return {
+      [day]: {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      },
+    };
+  }
+  return {
+    [day]: {
+      officeHour: `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`,
+      exhibition: animalsArray.map((animals) => animals.name),
+    },
+  };
+}
 
 function getSchedule(scheduleTarget) {
+  const weekDays = Object.keys(data.hours);
+
+  const infos = weekDays.reduce((acc, curr) => ({
+    ...getWeekSchedule(curr),
+    ...acc,
+  }), {});
+
   if ((Object.keys(infos)).includes(scheduleTarget)) { // se for um dia da semana
     return {
       [scheduleTarget]: infos[scheduleTarget],
     };
-  } if (Object.values(data.species).map((specie) => specie.name).includes(scheduleTarget)) { // se for um animal
+  }
+  if (Object.values(data.species).map((specie) => specie.name).includes(scheduleTarget)) { // se for um animal
     const daysSpec = data.species.find((specie) => specie.name === scheduleTarget).availability;
     return daysSpec;
   }
   return infos;
 }
-
-console.log(Object.keys(infos));
 
 module.exports = getSchedule;
